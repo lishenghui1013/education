93c6fbbe148e61223f49958e661a53a0ccddccf7
lishenghui_2008@126.com <lishenghui_2008@126.com>
2018-10-25 17:31:38 +0800

    第二次

--- Application/Admin/Controller/PublishAuditController.class.php
+++ Application/Admin/Controller/PublishAuditController.class.php
@@ -25,7 +25,7 @@ class PublishAuditController extends BaseController {
         $curr = $getInfo['curr'] ? $getInfo['curr'] : 1;//当前页
         $limit = $getInfo['limit'] ? $getInfo['limit'] : 1;//每页显示条数
         $start = ($curr-1) * $limit;//开始
-
+        $com_name = $getInfo['com_name'] ? strtotime($getInfo['com_name']) : '';//查询关键字
         $add_time = $getInfo['add_time'] ? strtotime($getInfo['add_time']) : '';//查询的时间
         $audit_status = $getInfo['audit_status'] ? $getInfo['audit_status'] : '';//查询的审核状态
         $where = array();
@@ -36,11 +36,14 @@ class PublishAuditController extends BaseController {
         if ($audit_status != '') {
             $where['s.audit_status'] = $audit_status;
         }
+        if ($com_name != '') {
+            $where['s.audit_status'] = array('like','%'.$com_name.'%');
+        }
         /*var_dump($where);exit;*/
         //查询总条数
-        $total = D('api_ct_users as s')->join('left join api_provinces as p on p.id=s.province_id')->join('left join api_cities as c on c.id=s.city_id')->join('left join api_areas as a on a.id=s.area_id')->join('left join api_user as u on u.id=s.audit_id')->where($where)->count();//查询满足要求的总记录数
+        $total = D('api_publish as p')->join('left join api_ct_users as s on s.id=p.pub_userid')->join('left join api_user as u on u.id=p.audit_userid')->where($where)->count();//查询满足要求的总记录数
 
-        $info = D('api_ct_users as s')->join('left join api_provinces as p on p.id=s.province_id')->join('left join api_cities as c on c.id=s.city_id')->join('left join api_areas as a on a.id=s.area_id')->join('left join api_user as u on u.id=s.audit_id')->field('s.id,s.user_name,s.phone,s.com_name,s.address,p.province,c.city,a.area,s.add_time,u.username,s.audit_time,s.audit_status')->where($where)->order('s.id')->limit($start, $limit)->select();
+        $info = D('api_publish as p')->join('left join api_ct_users as s on s.id=p.pub_userid')->join('left join api_user as u on u.id=p.audit_userid')->field('p.id,s.com_name,p.title,p.intro,p.pub_time,u.username,p.audit_time,p.audit_status')->where($where)->order('p.id')->limit($start, $limit)->select();
         foreach($info as $keys=>$values){
 
             foreach($values as $key=>$value){
@@ -80,15 +83,15 @@ class PublishAuditController extends BaseController {
     /**
      * 审核通过
      * @author: 李胜辉
-     * @time: 2018/10/23 09:32
+     * @time: 2018/10/25 14:32
      */
     public function open()
     {
         $id = I('post.id');
         $arr['audit_status'] = 'S';
-        $arr['audit_id'] = session('uid');
+        $arr['audit_userid'] = session('uid');
         $arr['audit_time'] = time();
-        $res = D('api_ct_users')->where(array('id' => $id))->save($arr);
+        $res = D('api_publish')->where(array('id' => $id))->save($arr);
         if ($res === false) {
             $this->ajaxError('操作失败');
         } else {
@@ -105,9 +108,9 @@ class PublishAuditController extends BaseController {
     {
         $id = I('post.id');
         $arr['audit_status'] = 'F';
-        $arr['audit_id'] = session('uid');
+        $arr['audit_userid'] = session('uid');
         $arr['audit_time'] = time();
-        $res = D('api_ct_users')->where(array('id' => $id))->save($arr);
+        $res = D('api_publish')->where(array('id' => $id))->save($arr);
         if ($res === false) {
             $this->ajaxError('操作失败');
         } else {
@@ -121,15 +124,75 @@ class PublishAuditController extends BaseController {
      */
     public function look()
     {
-        $id = I('post.id');
-        $arr['audit_status'] = 'S';
-        $arr['audit_id'] = session('uid');
-        $arr['audit_time'] = time();
-        $res = D('api_ct_users')->where(array('id' => $id))->save($arr);
-        if ($res === false) {
-            $this->ajaxError('操作失败');
+        $id = I('get.id');
+
+        $list = D('api_publish as p')->join('left join api_publish_content as c on c.publish_id=p.id')->join('left join api_ct_users as s on s.id=p.pub_userid')->field('p.title,p.price,p.cover,p.read_num,p.collect_num,p.share_num,p.user_type,p.item_type,p.intro,c.id,c.title,s.user_name,p.pub_time')->where(array('p.id' => $id))->find();
+        $this->assign('list', $list);
+        $this->display();
+    }
+    /**
+     * ajax查看目录
+     * @author: 李胜辉
+     * @time: 2018/10/25 09:32
+     */
+    public function ajaxLookCatalog()
+    {
+        $id = I('get.id');
+
+        $list = D('api_publish as p')->join('left join api_publish_content as c on c.publish_id=p.id')->join('left join api_ct_users as s on s.id=p.pub_userid')->field('p.title,p.price,p.cover,p.read_num,p.collect_num,p.share_num,p.user_type,p.item_type,p.intro,c.id,c.title,s.user_name,p.pub_time')->where(array('p.id' => $id))->find();
+        $this->assign('list', $list);
+        $this->display();
+    }
+    /**
+     * 查看目录详情
+     * @author: 李胜辉
+     * @time: 2018/10/25 09:32
+     */
+    public function lookCatalog()
+    {
+        $id = I('get.id');
+
+        $list = D('api_publish_content as c')->join('left join api_publish as p on p.id=c.publish_id')->join('left join api_ct_users as s on s.id=c.pub_userid')->field('p.title,c.price,c.title,c.content,c.read_num,c.share_num,c.collect_num,c.pub_time,s.user_name')->where(array('c.id' => $id))->find();
+        $this->assign('list', $list);
+        $this->display();
+    }
+    /**
+     * 图片上传
+     * @author: 李胜辉
+     * @time: 2018/10/17 17:32
+     */
+    public function upload()
+    {
+        //获取网站根目录地址$url
+        $PHP_SELF = $_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_SERVER['SCRIPT_NAME'];
+        $str = substr($PHP_SELF, 1);
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . '/' . substr($str, 0, strpos($str, '/') + 1);
+        if (!empty($_FILES)) {
+            $upload = new \Think\Upload();   // 实例化上传类
+            $upload->maxSize = 3145728;    // 设置附件上传大小
+            $upload->exts = array('jpg', 'gif', 'png', 'jpeg'); // 设置附件上传类型
+            $upload->rootPath = THINK_PATH;          // 设置附件上传根目录
+            $upload->savePath = '../Public/';    // 设置附件上传（子）目录
+            $upload->subName = 'uploads/articlePublish/knowledge';  //子文件夹
+            $upload->replace = true;  //同名文件是否覆盖
+            // 上传文件
+            $images = $upload->upload();
+            //return $images;
+            //Log::record('$images', Log::DEBUG);die;
+            //判断是否有图
+            if ($images) {
+                $info['code'] = 0;//成功
+                $info['msg'] = "";//信息
+                $info['data']['src'] = $url . substr($images['file']['savepath'], 3) . $images['file']['savename'];//拼接图片地址
+                echo json_encode($info);
+            } else {
+                $info['code'] = 1;//失败
+                $info['msg'] = "fail";//信息
+                $info['data']['src'] = $url . substr($images['file']['savepath'], 3) . $images['file']['savename'];//拼接图片地址
+                echo json_encode($info);
+            }
         } else {
-            $this->ajaxSuccess('操作成功');
+            echo json_encode(2);
         }
     }
 
