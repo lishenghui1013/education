0c110d83bf6889c31bcd257ba616e2ddf66916b4
lishenghui_2008@126.com <lishenghui_2008@126.com>
2018-10-29 13:53:43 +0800

    学生管理模块做完了

--- Application/Admin/Controller/WithdrawalAuditController.class.php
+++ Application/Admin/Controller/WithdrawalAuditController.class.php
@@ -16,60 +16,69 @@ class WithdrawalAuditController extends BaseController {
      */
     public function index()
     {
+        $this->display();
+    }
+    /**
+     * ajax列表页
+     * @author: 李胜辉
+     * @time: 2018/10/23 17:32
+     */
+    public function ajaxGetIndex()
+    {
+        $getInfo = I('post.');
+        $curr = $getInfo['curr'] ? $getInfo['curr'] : 1;//当前页
+        $limit = $getInfo['limit'] ? $getInfo['limit'] : 1;//每页显示条数
+        $start = ($curr-1) * $limit;//开始
+        $user_name = $getInfo['user_name'] ? $getInfo['user_name'] : '';//查询的审核状态
+        $phone = $getInfo['phone'] ? $getInfo['phone'] : '';//查询的审核状态
+        $e_status = $getInfo['e_status'] ? $getInfo['e_status'] : '';//查询的审核状态
+        $where = array();
 
-        $pagenum = I('post.pagenum/d', 1);//每页显示条数
-        $currentpage = I('get.p/d', 1);//当前页码
-        $user_name = I('post.user_name')!==''?I('post.user_name'):(I('get.buser_name')!==''?I('get.buser_name'):'');//用户名
-        $phone = I('post.phone')!==''?I('post.phone'):(I('get.bphone')!==''?I('get.bphone'):'');//电话
-        $status = I('post.e_status')!==''?I('post.e_status'):(I('get.ee_status')!==''?I('get.ee_status'):'');//审核状态
-        $map['e.etype'] = 2;//类型:提现
-        $map['e.r_type'] = 'S';//角色:学生
-        $map['b.audit_status'] = 'S';//银行卡审核状态通过
         if ($user_name != '') {
-            $map['b.user_name'] = $user_name;
+            $where['b.user_name'] = $user_name;
         }
         if ($phone != '') {
-            $map['b.phone'] = $phone;
+            $where['b.phone'] = $phone;
         }
-        if ($status != '') {
-            $map['e.e_status'] = $status;
+        if ($e_status != '') {
+            $where['e.e_status'] = $e_status;
         }
         //查询总条数
-        $count = D('api_expense as e')->join('left join api_users as s on s.id=e.user_id')->join('left join api_bankcard as b on b.id=e.bankcard_id')->join('left join api_user as u on u.id=e.audit_userid')->where($map)->count();//查询满足要求的总记录数
+        $total = D('api_expense as e')->join('left join api_users as s on s.id=e.user_id')->join('left join api_bankcard as b on b.id=e.bankcard_id')->join('left join api_user as u on u.id=e.audit_userid')->where($where)->count();//查询满足要求的总记录数
 
-        $list = D('api_expense as e')->join('left join api_users as s on s.id=e.user_id')->join('left join api_bankcard as b on b.id=e.bankcard_id')->join('left join api_user as u on u.id=e.audit_userid')->field('e.id,b.user_name,b.phone,s.balance,e.e_money,b.bank_name,b.bank_card,b.Acc_open,e.add_time,e.audit_time,u.username,e.e_status')->where($map)->order('e.id')->page($currentpage . ',' . $pagenum)->select();
+        $info = D('api_expense as e')->join('left join api_users as s on s.id=e.user_id')->join('left join api_bankcard as b on b.id=e.bankcard_id')->join('left join api_user as u on u.id=e.audit_userid')->field('e.id,b.user_name,b.phone,s.balance,e.e_money,b.bank_name,b.bank_card,b.Acc_open,e.add_time,e.audit_time,u.username,e.e_status')->where($where)->order('e.id')->limit($start, $limit)->select();
+        foreach($info as $keys=>$values){
 
-        $Page = new \Think\Page($count, $pagenum);// 实例化分页类 传入总记录数和每页显示的记录数
-        $Page->lastSuffix = false;//最后一页不显示为总页数
-        $Page->setConfig('header', '<li class="disabled hwh-page-info"><a>共<em>%TOTAL_ROW%</em>条  <em>%NOW_PAGE%</em>/%TOTAL_PAGE%页</a></li>');
-        $Page->setConfig('prev', '上一页');
-        $Page->setConfig('next', '下一页');
-        $Page->setConfig('last', '末页');
-        $Page->setConfig('first', '首页');
-        $Page->setConfig('theme', '%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
-        //分页跳转的时候保证查询条件
-        foreach ($map as $key => $val) {
-            $key = implode(explode('.',$key));
-            if($key=='_string'){
+            foreach($values as $key=>$value){
 
-                $temp_arr = explode(' ',$map[$key]);
-                foreach($temp_arr as $k=>$v){
-                    $temp_arr[$key] = trim($v);
+                if($values[$key]===null){
+                    $info[$keys][$key] = '';
+
                 }
-                $key = implode(explode('.',$temp_arr['0']));
-                $val = substr($temp_arr['2'],2,-2);
-
             }
-            $Page->parameter[$key] = urlencode($val);
 
         }
-        $show = $Page->show();// 分页显示输出
-        $this->assign('list', $list);//查询到的数据
-        $this->assign('page', $show);// 赋值分页输出
-        $this->assign('user_name', $user_name);//查询姓名
-        $this->assign('phone', $phone);// 查询手机号
-        $this->assign('status', $status);//查询状态
-        $this->display();
+        if($info){
+            $data = array(
+                'limit'=>$limit,
+                'curr'=>$curr,
+                'add_time'=>$getInfo['add_time'],
+                'status'=>'success',
+                'total' => $total,
+                'data' => $info
+            );
+        }else{
+            $data = array(
+                'limit'=>$limit,
+                'curr'=>$curr,
+                'add_time'=>$getInfo['add_time'],
+                'status'=>'fail',
+                'total' => $total,
+                'data' => $info
+            );
+        }
+
+        $this->ajaxReturn($data, 'json');
     }
 
     /**
