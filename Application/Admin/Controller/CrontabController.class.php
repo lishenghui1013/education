4f695741a47e986e8db24114b7ba615e16dfa935
lishenghui_2008@126.com <lishenghui_2008@126.com>
2018-11-20 18:09:00 +0800

    下班备份

--- Application/Admin/Controller/CrontabController.class.php
+++ Application/Admin/Controller/CrontabController.class.php
@@ -18,16 +18,31 @@ class CrontabController extends BaseController {
      */
     public function open()
     {
-        $id = I('post.id');
-        $arr['audit_status'] = 'S';
-        $arr['audit_id'] = session('uid');
-        $arr['audit_time'] = time();
-        $res = D('api_comment')->where(array('id' => $id))->save($arr);
-        if ($res === false) {
-            $this->ajaxError('操作失败');
-        } else {
-            $this->ajaxSuccess('操作成功');
+        ignore_user_abort();//关掉浏览器，PHP脚本也可以继续执行.
+        set_time_limit(0);
+        $date = strtotime(date('Y-m-d',time()));
+        $time = 60*24*60;
+        $control = include 'stop.php';
+        $i = 1;
+
+        while ($control) {
+
+            $control = include 'stop.php';
+            $conn = new mysqli("localhost", "root", "root", "education");
+            $sql = 'update api_users set balance=(balance+1) where id=1';
+            sleep($time);
+            $conn->query($sql);
         }
+        do{
+            $timing = M('one_timing')->where(array('id'=>1))->find();//通过后台控制数据库的数据 来控制此循环
+            ignore_user_abort();//关掉浏览器，PHP脚本也可以继续执行.
+            set_time_limit(0);// 通过set_time_limit(0)可以让程序无限制的执行下去
+            $interval=60*60*24;// 每隔24小时运行
+            //todo
+            echo time()."<br>";
+            sleep($interval);
+        }
+        while($timing['timing'] == 1);//当为true时  无限循环
     }
 
 
